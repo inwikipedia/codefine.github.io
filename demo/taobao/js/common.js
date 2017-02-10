fb648e8f11a73cbb51054603bdcc6661ba33bdf5
codefine <you@277133779@qq.com>
2017-02-10 17:09:04 +0800

    demo
    demo-taobao

---
+++ demo/taobao/js/common.js
@@ -0,0 +1,155 @@
+//添加class
+function addClass(obj,iClass){
+	var oriClass = obj.className.split(' ');
+	var newClass = iClass.split(' ');
+	for (var i=0; i<oriClass.length; i++) {
+		for (var j=0; j<newClass.length; j++) {
+			if (newClass[j]==oriClass[i]) {
+				newClass.splice(j,1);
+			}
+		}
+	}
+	for (var i=0; i<newClass.length; i++) {
+		oriClass.push(newClass[i]);
+	}
+	obj.className = oriClass.join(' ');
+}
+
+//删除class
+function removeClass(obj,iClass){
+	var oriClass = obj.className.split(' ');
+	var newClass = iClass.split(' ');
+	for (var i=0; i<oriClass.length; i++) {
+		for (var j=0; j<newClass.length; j++) {
+			if (newClass[j]==oriClass[i]) {
+				oriClass.splice(i,1);
+			}
+		}
+	}
+	obj.className = oriClass.join(' ');
+}
+
+//获取元素到页面边界的距离
+function getOffset(obj){
+	var pos = {
+		"left": 0,
+		"top": 0
+	};
+	while (obj) {
+		pos.left += obj.offsetLeft;
+		pos.top += obj.offsetTop;
+		obj = obj.offsetParent;
+	}
+	return pos;
+}
+
+//缓冲滚动
+function easeScroll(iTarget){
+	clearInterval(document.body.timer);
+	var iStatus = 0;
+	var iSpeed = 0;
+	document.body.timer = setInterval(function (){
+		iStatus = document.documentElement.scrollTop||document.body.scrollTop;
+		if (iStatus>iTarget) {
+			iSpeed = Math.floor((iTarget-iStatus)*0.1);
+		} else {
+			iSpeed = Math.ceil((iTarget-iStatus)*0.1);
+		}
+		if (iStatus==iTarget) {
+			clearInterval(document.body.timer);
+		} else {
+			document.documentElement.scrollTop = document.body.scrollTop = iStatus + iSpeed;
+		}
+	},14);
+}
+
+//水平轮播
+function xTab(obj,time){
+	var tabCont = obj.querySelector('.tab-main');
+	var aTabBtn = obj.querySelector('.tab-count').querySelectorAll('span');
+	var oLeft = obj.querySelector('.btn-left');
+	var oRight = obj.querySelector('.btn-right');
+	var iDis = obj.clientWidth;
+	var oPage = obj.querySelector('.page');
+	var iNow = 0;
+	indexActive();
+	dirActive();
+	autoPlay(time);
+	//自动播放
+	function autoPlay(time){
+		obj.onmouseover = function (){
+			clearInterval(obj.play);
+		}
+		obj.onmouseout = function (){
+			autoPlay(time);
+		}
+		obj.play = setInterval(function (){
+			iNow++;
+			tabMove(iNow);
+		},time);
+	}
+	//序号按钮响应
+	function indexActive(){
+		for (var i=0; i<aTabBtn.length; i++) {
+			(function (n){
+				aTabBtn[n].onclick = function (){
+					iNow = n;
+					tabMove(iNow);
+				};
+			})(i);
+		}
+	};
+	//方向按钮响应
+	function dirActive(){
+		oLeft.onclick = function (){
+			iNow--;
+			tabMove(iNow);
+		}
+		oRight.onclick = function (){
+			iNow++;
+			tabMove(iNow);
+		}
+	};
+	//缓冲轮播运动
+	function tabMove(n){
+		switch (n) {
+			case -2:
+				n = iNow = aTabBtn.length-2;
+				tabCont.style.left = -aTabBtn.length*iDis + 'px';
+				break;
+			case aTabBtn.length:
+				n = iNow = 0;
+				tabCont.style.left = 0 + 'px';
+				break;
+		}
+		if (oPage) {
+			oPage.innerHTML = n + 1;
+		}
+		clearInterval(obj.timer);
+		for (var i=0; i<aTabBtn.length; i++) {
+			removeClass(aTabBtn[i],'active');
+		}
+		if (n==-1) {
+			addClass(aTabBtn[aTabBtn.length-1],'active');
+		} else {
+			addClass(aTabBtn[n],'active');
+		}
+		var iOrig = tabCont.offsetLeft;
+		var iTarget = -iDis*(n+1);
+		obj.timer = setInterval(function (){
+			var iStatus = tabCont.offsetLeft;
+			if (iTarget<iStatus) {
+				var iSpeed = Math.floor((iTarget-iStatus)*0.1);
+			} else {
+				var iSpeed = Math.ceil((iTarget-iStatus)*0.1);
+			}
+			if (iStatus==iTarget) {
+				clearInterval(obj.timer);
+				tabCont.style.left = iTarget + 'px';
+			}
+			tabCont.style.left = iStatus + iSpeed + 'px';
+		},14);
+	}
+};
+
+
---
+++ demo/taobao/js/main.js
@@ -0,0 +1,185 @@
+window.onload = function (){
+	
+	var oTbTab = document.querySelector('.tb-banner');
+	var oTmTab = document.querySelector('.tm-banner');
+	var aSearch = document.querySelectorAll('.search-bar');
+	
+	fixSide();
+	qrClose();
+	xTab(oTbTab,4000);
+	xTab(oTmTab,4500);
+	searchList();
+	tabType();
+	
+	window.onscroll = function (){
+		fixToggle();
+		fixSide();
+	}
+	
+}
+
+//顶部固定搜索框
+function fixToggle(){
+	var fixSearch = document.querySelector('#fix-search');
+	var oFix = document.querySelector('.fix-search-bar');
+	var oMain = document.querySelector('.main-search-bar');
+	var oBar = document.querySelector('.search-bar');
+	var oIns = document.querySelector('.search-sug');
+	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	if (scrollTop>125) {
+		addClass(oBar,'fl');
+		oFix.appendChild(oBar);
+		fixSearch.style.display = 'block';
+	} else {
+		removeClass(oBar,'fl');
+		oMain.insertBefore(oBar,oIns);
+		fixSearch.style.display = 'none';
+	}
+}
+
+//侧边导航
+function fixSide(){
+	var oSidebar = document.querySelector('#sidebar');
+	var oOften = oSidebar.querySelector('.sidebar-often');
+	var oFashion = oSidebar.querySelector('.sidebar-fashion');
+	var oQuality = oSidebar.querySelector('.sidebar-quality');
+	var oFeature = oSidebar.querySelector('.sidebar-feature');
+	var oBenefit = oSidebar.querySelector('.sidebar-benefit');
+	var oGuess = oSidebar.querySelector('.sidebar-guess');
+	var oBacktop = oSidebar.querySelector('.sidebar-backtop');
+	//绝对/固定定位转换
+	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	if (scrollTop>440) {
+		oSidebar.style.position = 'fixed';
+		oSidebar.style.top = '49px';
+	} else {
+		oSidebar.style.position = 'absolute';
+		oSidebar.style.top = '488px';
+	}
+	//按钮滚动响应
+	var disH = 49; //顶部固定定位搜索条高度
+	var tOften = getOffset(document.querySelector('.section-often')).top-disH;
+	var tFashion = getOffset(document.querySelector('.section-fashion')).top-disH;
+	var tQuality = getOffset(document.querySelector('.section-quality')).top-disH;
+	var tFeature = getOffset(document.querySelector('.section-feature')).top-disH;
+	var tBenefit = getOffset(document.querySelector('.section-benefit')).top-disH;
+	var tGuess = getOffset(document.querySelector('.main-guess')).top-disH;
+	//返回顶部响应
+	if (scrollTop>800-disH) {
+		oBacktop.style.display = 'block';
+	} else {
+		oBacktop.style.display = 'none';
+	}
+	//主要按钮响应
+	var elts = oSidebar.querySelectorAll('li');
+	for (var i=0; i<elts.length; i++) {
+		removeClass(elts[i],'active');
+	}
+	if (scrollTop<tFashion) {
+		removeClass(oFashion,'active');
+		addClass(oOften,'active');
+	} else if (scrollTop>=tFashion&&scrollTop<tQuality) {
+		removeClass(oOften,'active');
+		removeClass(oQuality,'active');
+		addClass(oFashion,'active');
+	} else if (scrollTop>=tQuality&&scrollTop<tFeature) {
+		removeClass(oFashion,'active');
+		removeClass(oFeature,'active');
+		addClass(oQuality,'active');
+	} else if (scrollTop>=tFeature&&scrollTop<tBenefit) {
+		removeClass(oQuality,'active');
+		removeClass(oBenefit,'active');
+		addClass(oFeature,'active');
+	} else if (scrollTop>=tBenefit&&scrollTop<tGuess) {
+		removeClass(oFeature,'active');
+		removeClass(oGuess,'active');
+		addClass(oBenefit,'active');
+	} else if (scrollTop>=tGuess) {
+		removeClass(oBenefit,'active');
+		addClass(oGuess,'active');
+	}
+	//点击页面滚动到对应位置
+	oOften.onclick = function (){
+		easeScroll(tOften);
+	}
+	oFashion.onclick = function (){
+		easeScroll(tFashion);
+	}
+	oQuality.onclick = function (){
+		easeScroll(tQuality);
+	}
+	oFeature.onclick = function (){
+		easeScroll(tFeature);
+	}
+	oBenefit.onclick = function (){
+		easeScroll(tBenefit);
+	}
+	oGuess.onclick = function (){
+		easeScroll(tGuess);
+	}
+	oBacktop.onclick = function (){
+		easeScroll(0);
+	}
+}
+
+//关闭主二维码
+function qrClose(){
+	var oQR = document.querySelector('#closeQR');
+	oQR.onclick = function (){
+		this.parentNode.style.display = 'none';
+	};
+}
+
+//JsonP
+function jsonp(data){
+	var oUl = document.querySelector('.search-list');
+	var res = data.result;
+	if (res.length) {
+		var html = '';
+		for (var i=0; i<res.length; i++) {
+			html += '<li><a target="_blank" href="https://s.taobao.com/search?q='+res[i][0]+'">'+res[i][0]+'</a></li>';
+		}
+		oUl.innerHTML = html;
+		oUl.style.display = 'block';
+	} else {
+		oUl.style.display = 'none';
+	}
+}
+
+//搜索框智能推荐
+function searchList(){
+	var oBar = document.querySelector('.search-bar');
+	var oUl = oBar.querySelector('.search-list');
+	var oKw = oBar.querySelector('input');
+	var oP = oBar.querySelector('p');
+	oKw.onkeyup = function (){
+		if (this.value!='') {
+			var oScript = document.createElement('script');
+			oScript.src = 'https://suggest.taobao.com/sug?code=utf-8&q='+this.value+'&callback=jsonp';
+			document.body.appendChild(oScript);
+			oP.style.display = 'none';
+		} else {
+			oUl.style.display = 'none';
+			oP.style.display = 'block';
+		}
+	}
+}
+
+//搜索类型切换(伪)
+function tabType(){
+	var aType = document.querySelectorAll('.main-search-bar>.search-type>li');
+	var oInp = document.querySelector('.main-search-bar input');
+	var oBtn = document.querySelector('.main-search-bar button');
+	var aColor = ['#ff5400','#C80000','#FF5300'];
+	for (var i=0; i<aType.length; i++) {
+		(function (n){
+			aType[n].onclick = function (){
+				for (var i=0; i<aType.length; i++) {
+					removeClass(aType[i],'active');
+				}
+				addClass(this,'active');
+				oInp.style.borderColor = oBtn.style.backgroundColor = aColor[n];
+			};
+		})(i);
+	}
+}
